8481616678390cc05046b43cdbe4645472caa1cc
istng <ivanstng@gmail.com>
2019-07-17 17:33:28 -0300

    working, further work needed

--- instagramFeedBot.py
+++ instagramFeedBot.py
@@ -5,7 +5,7 @@ import logging
 from datetime import datetime, timedelta
 import argparse
 import re
-import instagramFeed as igf
+import instagramAccountFeed as igaf
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,8 +18,14 @@ checkTimeArgHelp = 'time to check for new publications'
 userIdArgHelp    = 'telegram user id'
 
 
-startMsg = 'Hello! Im the Instagram Feed bot, you can find out what I can do with /help'
-helpMsg  = 'Here is going to be a brief description of what I can do.' #maybe make functions to ask what each one does separately
+startMsg           = 'Hello! Im the Instagram Feed bot, you can find out what I can do with /help'
+helpMsg            = 'Here is going to be a brief description of what I can do.' #maybe make functions to ask what each one does separately
+addedAccounstMsg   = 'Accounts successfully added!'
+addedKeywordsMsg   = 'Keywords successfully added!'
+deletedAccountsMsg = 'Accounts successfully deleted.'
+deletedKeywordsMsg = 'Keywords successfully deleted.'
+enabledAllMsg      = 'All posts enabled!'
+enabledKeywordsMsg = 'Only posts containing keywords enabled!'
 
 
 checkTimeDefault = 24 #in hours
@@ -43,40 +49,65 @@ def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=helpMsg)
 
 
-def add_account(bot, update):
-    return
+def add_accounts(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='hola')
+    accounts = update.message.text.split()[1::]
+    for account in accounts:
+         if account not in accountsFeed: accountsFeed[account] = igaf.account(account, datetime(year=2018, month=3, day=1))
+    bot.send_message(chat_id=update.message.chat_id, text=addedAccounstMsg)
 
 
 def add_keywords(bot, update):
-    return
+    userMsg = update.message.text.split()[1::]
+    account = userMsg[0]
+    keywords = set(userMsg[1::])
+    accountsFeed[account].add_keywords(keywords)
+    bot.send_message(chat_id=update.message.chat_id, text=addedKeywordsMsg)
 
 
 def list_accounts(bot, update):
-    return
+    accountsNames = '\n'.join(accountsFeed.keys())
+    bot.send_message(chat_id=update.message.chat_id, text=accountsNames)
 
 
 def list_keywords(bot, update):
-    return
+    account = update.message.text.split()[1::]
+    keywords = accountsFeed[account].get_keywords()
+    bot.send_message(chat_id=update.message.chat_id, text=keywords)    
 
 
 def delete_account(bot, update):
-    return
+    accounts = update.message.text.split()[1::]
+    for account in accounts:
+         if account in accountsFeed: del accountsFeed[account]
+    bot.send_message(chat_id=update.message.chat_id, text=deletedAccountsMsg)
 
 
 def delete_keywords(bot, update):
-    return
+    userMsg = update.message.text.split()[1::]
+    account = userMsg[0]
+    keywords = set(userMsg[1::])
+    accountsFeed[account].delete_keywords(keywords)
+    bot.send_message(chat_id=update.message.chat_id, text=deletedKeywordsMsg)
 
 
 def enable_all(bot, update):
-    return
+    account = update.message.text.split()[1::]
+    if accountsFeed[account].are_keywords_enabled(): accountsFeed[account].toggle_keywords()
+    bot.send_message(chat_id=update.message.chat_id, text=enabledAllMsg)
 
 
 def enable_keywords(bot, update):
-    return
+    account = update.message.text.split()[1::]
+    if not accountsFeed[account].are_keywords_enabled(): accountsFeed[account].toggle_keywords()
+    bot.send_message(chat_id=update.message.chat_id, text=enabledKeywordsMsg)
 
 
 def check_feed(bot, job):
-    job.context.message.reply_text('test')
+    for account in accountsFeed:
+        newPosts = accountsFeed[account].get_last_posts(1)
+        for post in newPosts:
+            job.context.message.reply_text(post)
 
 
 #it should enter always, not with the filter text...
@@ -88,12 +119,15 @@ def main():
     global botArgs
     botArgs = parse_input()
 
+    global accountsFeed
+    accountsFeed = dict()
+
 
     updater = Updater(botArgs.token)
-    updater.dispatcher.add_handler(MessageHandler(Filters.all, check_feed_job, pass_job_queue=True))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, check_feed_job, pass_job_queue=True))
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("help", help))
-    updater.dispatcher.add_handler(CommandHandler("addaccount", add_account))
+    updater.dispatcher.add_handler(CommandHandler("addaccount", add_accounts))
     updater.dispatcher.add_handler(CommandHandler("addkeywords", add_keywords))
     updater.dispatcher.add_handler(CommandHandler("listaccounts", list_accounts))
     updater.dispatcher.add_handler(CommandHandler("listkeywords", list_keywords))
