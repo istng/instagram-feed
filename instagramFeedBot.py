0d9a038561362f0038a843425866c5f694b1f8de
istng <ivanstng@gmail.com>
2019-07-26 11:17:41 -0300

    add user msg and errors logs

--- instagramFeedBot.py
+++ instagramFeedBot.py
@@ -15,6 +15,7 @@ argumentsDescMsg = 'Bot initialization parameters.'
 tokenArgHelp     = 'telegram token'
 economyDBHelp    = 'economy DB path'
 checkTimeArgHelp = 'time to check for new publications'
+numbOfPostsHelp  = 'posts to check from instagram account'
 userIdArgHelp    = 'telegram user id'
 
 
@@ -26,7 +27,8 @@ noParamsGiven      = 'Please send the command with valid parameters.'
 functionsHelp = "/addaccounts username1 username2 username3 ...\nAdds one or more accounts by their usernames.\n\n/addkeywords username keyword1 keyword2 ...\nAdds one or more keywords to the username's account.\n\n/deleteaccounts username1 username2 username3 ...\nDeletes all accounts with given usernames.\n\n/deletekeywords username keyword1 keyword2 ...\nDeletes all given keywords from given username's account.\n\n/enableall username1 username2 username3 ...\nEnables all posts from each username's account.\n\n/enablekeywords username1 username2 username3 ...\nEnables only posts containing each username's account keywords.\n\n/listaccounts\nI'll send a list of all present accounts.\n\n/listkeywords username\nI'll send a list of all the keywords of that username's account."
 
 
-checkTimeDefault = 24 #in hours
+checkTimeDefault = 12*60*60 #in seconds
+numberOfPostsDef = 5
 
 
 def parse_input():
@@ -35,9 +37,23 @@ def parse_input():
     parser.add_argument('token', metavar='TOKEN', type=str, help=tokenArgHelp)
     parser.add_argument('economyDB', metavar='ECONOMY DB', type=str, help=economyDBHelp)
     parser.add_argument('-ct', metavar='CHECK TIME', type=int, default=checkTimeDefault, help=checkTimeArgHelp)
+    parser.add_argument('-np', metavar='NUMBER OF POSTS', type=int, default=numberOfPostsDef, help=numbOfPostsHelp)
     parser.add_argument('-uid', metavar='USER ID', type=int, help=userIdArgHelp)
     args = parser.parse_args()
     return args
+
+
+def log_user_msg(func):
+    def log_and_call(bot, update):
+        chatId = update.message.chat_id
+        params = update.message.text
+        logging.info(str(chatId)+', '+params)
+        return func(bot, update)
+    return log_and_call
+
+
+def log_errors(userId, errors):
+    if errors!=[]: logging.error(str(userId)+', '+str(errors))
 
 
 def check_user_msg_parameters(func):
@@ -56,6 +72,7 @@ def process_reply_msg(errors):
     return replyMsg
 
 
+@log_user_msg
 @check_user_msg_parameters
 def add_accounts(bot, update):
     userId = update.message.chat_id
@@ -66,9 +83,11 @@ def add_accounts(bot, update):
             instagramFeeder.add_account(userId, username)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
 
 
+@log_user_msg
 @check_user_msg_parameters
 def add_keywords(bot, update):
     userId  = update.message.chat_id
@@ -81,17 +100,22 @@ def add_keywords(bot, update):
             instagramFeeder.add_keyword(userId, username, keyword)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
 
 
+@log_user_msg
 def list_username_accounts(bot, update):
-    chatId = update.message.chat_id
-    usernames = [u for u in instagramFeeder.list_usernames(chatId)]
+    userId = update.message.chat_id
+    usernames = [u for u in instagramFeeder.list_usernames(userId)]
     replyMsg = '\n'.join(usernames)
-    if len(usernames)==0: replyMsg = 'There are no accounts added yet.'
-    bot.send_message(chat_id=chatId, text=replyMsg)
+    if len(usernames)==0:
+        replyMsg = 'There are no accounts added yet.'
+        logging.warning(str(userId)+', '+replyMsg)
+    bot.send_message(chat_id=userId, text=replyMsg)
 
 
+@log_user_msg
 @check_user_msg_parameters
 def list_keywords(bot, update):
     userId = update.message.chat_id
@@ -101,9 +125,11 @@ def list_keywords(bot, update):
         replyMsg = ' '.join([k for k in instagramFeeder.list_keywords(userId, username)])
     except ValueError as e:
         replyMsg = str(e)
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=replyMsg)
 
 
+@log_user_msg
 @check_user_msg_parameters
 def delete_accounts(bot, update):
     userId = update.message.chat_id
@@ -114,9 +140,11 @@ def delete_accounts(bot, update):
             instagramFeeder.delete_account(userId, username)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
 
 
+@log_user_msg
 @check_user_msg_parameters
 def delete_keywords(bot, update):
     userId  = update.message.chat_id
@@ -129,9 +157,11 @@ def delete_keywords(bot, update):
             instagramFeeder.delete_keyword(userId, username, keyword)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
 
 
+@log_user_msg
 def enable_all(bot, update):
     userId = update.message.chat_id
     usernames = update.message.text.split()[1::]
@@ -141,9 +171,11 @@ def enable_all(bot, update):
             instagramFeeder.enable_all(userId, username)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
     
 
+@log_user_msg
 def enable_keywords(bot, update):
     userId = update.message.chat_id
     usernames = update.message.text.split()[1::]
@@ -153,13 +185,16 @@ def enable_keywords(bot, update):
             instagramFeeder.enable_keywords(userId, username)
         except ValueError as e:
             errors.append(str(e))
+    log_errors(userId, errors)
     bot.send_message(chat_id=userId, text=process_reply_msg(errors))
 
 
+@log_user_msg
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=startMsg)
 
 
+@log_user_msg
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=helpMsg+functionsHelp)
 
@@ -170,8 +205,9 @@ def check_feed(bot, job):
     for feedee in feedees:
         usernames = instagramFeeder.list_usernames(feedee)
         for username in usernames:
-            posts = instagramFeeder.get_last_posts(feedee, username, 1)
+            posts = instagramFeeder.get_last_posts(feedee, username, botArgs.np)
             for post in posts:
+                logging.info(str(feedee.feedId)+', '+post)
                 job.context.message.reply_text(post)
 
 
@@ -181,6 +217,7 @@ def check_feed_job(bot, update, job_queue):
 
 
 def main():
+    logging.info('Started running')
     global botArgs
     botArgs = parse_input()
 
